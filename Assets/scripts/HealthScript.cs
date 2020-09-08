b63794fb08b43987e7020d45cb7c65a08dce65b9
Kotenka <37335759+Kotenka@users.noreply.github.com>
2020-09-08 16:32:45 +0300

    звукВыстрела

--- Assets/scripts/HealthScript.cs
+++ Assets/scripts/HealthScript.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(AudioSource))]
 public class HealthScript : MonoBehaviour
 {
     //всего хитпоитов
     public int hp = 1;
     //враг или игрок?
     public bool isEnemy = true;
+
+    public AudioClip BoyUh;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     //наносим урон и проверяем должен ли объект быть уничтоженым
     public void Damage (int damageCount)
     {
@@ -28,10 +36,16 @@ public class HealthScript : MonoBehaviour
             //избегайте дружественного огня
             if (shot.isEnemyShot != isEnemy)
             {
+                audioSource.PlayOneShot(BoyUh, 0.7F);
                 Damage(shot.damage);
                 //уничтожить выстрел
                 Destroy(shot.gameObject);
                 GameObject go = GameObject.FindGameObjectWithTag("Player");
+               
+                //GetComponent<AudioSource>().PlayOneShot(BoyUh);
+                //GameObject enemy = GameObject.FindGameObjectWithTag("Enemy");
+                //enemy.GetComponent<AudioSource>().PlayOneShot(BoyUh);
+                //audio.PlayOneShot(audio.clip);
                 go.GetComponent<ScoreScript>().AddScore();
                 
               //  ScoreScript.scoreValue += 10;
